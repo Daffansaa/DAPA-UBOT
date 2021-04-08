6370712fa06cf2663d1297960f180fb41864c9f8
ramadhani892 <80225366+ramadhani892@users.noreply.github.com>
2021-04-09 02:56:52 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -349,7 +349,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} ♛".format("♛", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} ㊙️".format("㊙️", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -365,10 +365,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         ] + [
             (
                 custom.Button.inline(
-                    "☚", data="{}_prev({})".format(prefix, modulo_page)
+                    "👈", data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 custom.Button.inline(
-                    "☛", data="{}_next({})".format(prefix, modulo_page)
+                    "👉", data="{}_next({})".format(prefix, modulo_page)
                 )
             )
         ]
@@ -392,7 +392,7 @@ with bot:
             if event.message.from_id != uid:
                 await event.reply("RAM-UBOT, Buat Userbot Mu Sendiri [Tekan Disini](https://github.com/ramadhani892/RAM-UBOT.git)")
             else:
-                await event.reply(f"`Hai Lord {ALIVE_NAME}\n\nApa Kabarmu?`")
+                await event.reply(f"`Hai {ALIVE_NAME}\n\nApa Kabarmu?`")
 
         @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
         async def inline_handler(event):
@@ -404,7 +404,7 @@ with bot:
                 result = builder.article(
                     "Harap Gunakan .help Untuk Perintah",
                     text="{}\n\n**❃ Jumlah Modul Yang Tersedia:** `{}`\n               \n**❃ Daftar Modul RAM-UBOT:** \n".format(
-                        "**♛ RAM-UBOT**",
+                        "**㊙️ RAM-UBOT**",
                         len(dugmeler),
                     ),
                     buttons=buttons,
@@ -419,11 +419,11 @@ with bot:
             else:
                 result = builder.article(
                     "**RAM-UBOT**",
-                    text="""**Anda Bisa Membuat RAM-UBOT Anda Sendiri Dengan Cara:** [Tekan Disini](t.me/Lorduserbot_Group)""",
+                    text="""**Anda Bisa Membuat RAM-UBOT Anda Sendiri Dengan Cara:** [Tekan Disini](https://github.com/ramadhani892/RAM-UBOT)""",
                     buttons=[
                         [
                             custom.Button.url(
-                                "Repo Lord-Userbot",
+                                "Repo RAM-UBOT",
                                 "https://github.com/ramadhani892/RAM-UBOT.git"),
                             custom.Button.url(
                                 "Pemilik Repo",
@@ -496,7 +496,7 @@ with bot:
                     )
                 )
             else:
-                reply_pop_up_alert = f"Harap Deploy Lord Userbot Anda Sendiri, Jangan Menggunakan Milik Lord {ALIVE_NAME} ツ"
+                reply_pop_up_alert = f"Harap Deploy RAM-UBOT Anda Sendiri, Jangan Menggunakan Milik {ALIVE_NAME} ツ"
 
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
