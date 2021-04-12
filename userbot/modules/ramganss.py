147d0f394ff0da8fddcc46edac7b7428d9550b69
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-04-12 07:15:52 +0700

    Update ramganss.py

--- userbot/modules/ramganss.py
+++ userbot/modules/ramganss.py
@@ -394,6 +394,15 @@ async def typewriter(typew):
                      "@TEMAN_RANDOM\n"
                      "@TEMAN_RANDOM\n"
                      "@TEMAN_RANDOM")
+
+
+
+@register(outgoing=True, pattern='^.gi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Mutualan Yukkk!...**")
+    sleep(2)
+    await typew.edit("INSTAGRAM= [TEKAN](https://www.instagram.com/ramadh20?r=nametag)")
 CMD_HELP.update({
     "vip":
     "`.bulan` ; `.hati` ; `.gbn` ; `.tolol` ; `.gmt`\
