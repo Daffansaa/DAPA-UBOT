c24c3952d7b1d7b979d7253cdeeac54940d763a5
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-04-16 17:26:57 +0700

    Update ramganss.py

--- userbot/modules/ramganss.py
+++ userbot/modules/ramganss.py
@@ -437,7 +437,7 @@ CMD_HELP.update({
     \nUsage: liat sendiri.\
     \n\n`.y` ; `.uasu`\
     \nUsage: jempol & Cek dyno\
-    \n\n`.tawa` ; `.oy` ; `.fck` ; `.tp` ; `As`\
+    \n\n`.tawa` ; `.oy` ; `.fck` ; `.tp` ; `.as`\
     \nUsage: ketawa lari , Nyuruh nimbrung , fvck & Coba sendiri.\
     \n\n`.ular` ; `.babi` ; `.ajg`\
     \nUsage: liat sendiri."
