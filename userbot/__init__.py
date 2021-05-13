037457d9cb7ca97bdb762e7fbf96b39a097102f3
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-14 05:09:25 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -319,7 +319,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_message(BOTLOG_CHATID, "```║RAM-UBOT TELAH AKTIF!║\n\n Jika Tidak Bisa Di .ping Silahkan Anda Cek Viewlogs Pada heroku Anda.```")
+    await bot.send_message(BOTLOG_CHATID, "```║RAM-UBOT TELAH AKTIF!║\n\n ▰▰▰▰▰▰▰▰▰▰▰\nJika Tidak Bisa Di .ping Silahkan Anda Cek Viewlogs Pada heroku Anda. \n\n▰▰▰▰▰▰▰▰▰▰▰```")
     return
 
 with bot:
