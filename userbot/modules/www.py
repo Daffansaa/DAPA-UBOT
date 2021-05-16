1a6ab0e0ac3fd87cd26950e538902a0a4f7cd803
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-16 13:45:30 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -194,6 +194,7 @@ async def pingme(pong):
     await pong.edit("PONG")
     await asyncio.sleep(1)
     await pong.edit("✨")
+    await asyncio.sleep(2)
     end = datetime.now()
     duration = (end - start).microseconds / 9000
     await pong.edit(f"**Oᴡɴᴇʀ : {ALIVE_NAME}**\n`%sms`" % (duration))
