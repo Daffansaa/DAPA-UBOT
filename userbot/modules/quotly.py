018e274764d5994413eb07a2b0df92fc073e6d7e
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-17 14:41:58 +0700

    Update quotly.py

--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -47,13 +47,13 @@ async def quotess(qotli):
     if qotli.fwd_from:
         return
     if not qotli.reply_to_msg_id:
-        return await qotli.edit("```Mohon Balas Ke Pesan Lord```")
+        return await qotli.edit("```Mohon Balas Ke Pesan!!```")
     reply_message = await qotli.get_reply_message()
     if not reply_message.text:
-        return await qotli.edit("```Mohon Balas Ke Pesan Lord```")
+        return await qotli.edit("```Mohon Balas Ke Pesan!!```")
     chat = "@QuotLyBot"
     if reply_message.sender.bot:
-        return await qotli.edit("```Mohon Balas Ke Pesan Lord```")
+        return await qotli.edit("```Mohon Balas Ke Pesan!!```")
     await qotli.edit("```Sedang Memproses Sticker, Mohon Menunggu ツ```")
     try:
         async with bot.conversation(chat) as conv:
