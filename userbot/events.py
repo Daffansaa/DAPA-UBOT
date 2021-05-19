2a2a03f281a3735494cd65af99e8fe764a29dc39
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-20 02:16:49 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,11 +97,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Lord-Userbot ERROR**\n"
-                    link = "Silahkan chat: @liualvinas"
+                    text = "**RAM-UBOT ERROR**\n"
+                    link = "Silahkan chat: @MAAFGAUSAHSOKAP"
                     text += "Untuk melaporkan kesalahan"
                     text += f"- tinggal teruskan pesan ini {link}.\n"
-                    text += "Alvin Siap Membantu Kamu\n"
+                    text += "Rama Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
