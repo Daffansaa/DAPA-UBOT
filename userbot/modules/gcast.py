5b13e6eb863db807986ec362176339b5a116f6c1
Rama <80225366+ramadhani892@users.noreply.github.com>
2021-05-24 02:52:56 +0700

    Update gcast.py

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -1,21 +1,15 @@
-# frm Ultroid
-# port by Koala @manusiarakitann
-# @LordUserbot_Group
-# Rama Ganteng mks sm sm
-
 from userbot.events import register
 from userbot import CMD_HELP, bot
-# Rama Ganteng mks sm sm
 
 
 @register(outgoing=True, pattern="^.gcast (.*)")
 async def gcast(event):
     xx = event.pattern_match.group(1)
     if not xx:
-        return await event.edit("`Teks nya mana tolol!!`")
+        return await event.edit("`Mohon Berikan Sebuah Pesan`")
     tt = event.text
     msg = tt[6:]
-    kk = await event.edit("`Sedang Mengirim Pesan Secara Global... ⭐`")
+    kk = await event.edit("`Sedang Mengirim Pesan Secara Global... 📢`")
     er = 0
     done = 0
     async for x in bot.iter_dialogs():
@@ -28,9 +22,33 @@ async def gcast(event):
                 er += 1
     await kk.edit(f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**")
 
-# Rama Ganteng mks sm sm
+
+@register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if not xx:
+        return await event.edit("`Berikan beberapa teks untuk Siaran Global`")
+    tt = event.text
+    msg = tt[7:]
+    kk = await event.edit("`Sedang Mengirim pesan secara global...`")
+    er = 0
+    done = 0
+    async for x in bot.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await bot.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(f"Berhasil Mengirim Pesan Ke `{done}` obrolan, kesalahan dalam `{er}` obrolan(s)")
+
+
 CMD_HELP.update(
     {
-        "gcast": "`.gcast <pesan>`\
-    \nPenjelasan: Global Broadcast mengirim pesan ke Seluruh Grup yang kamu Masuki."
-    })
+        "broadcast": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gcast`\
+         \n↳ : Mengirim Pesan Group Secara Global.\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.gucast`\
+         \n↳ : Mengirim Pesan Pribadi Secara Global"
+    }
+)
