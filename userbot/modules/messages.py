c52538c4622ecc270e67fe1a248d05f2ed5bddf3
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-05-27 21:20:29 +0700

    Update messages.py

--- userbot/modules/messages.py
+++ userbot/modules/messages.py
@@ -12,7 +12,7 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r"^\.purge$")
+@register(outgoing=True, pattern=r"^\Purge$")
 async def fastpurger(purg):
     chat = await purg.get_input_chat()
     msgs = []
@@ -45,7 +45,7 @@ async def fastpurger(purg):
     await done.delete()
 
 
-@register(outgoing=True, pattern=r"^\.purgeme")
+@register(outgoing=True, pattern=r"^\Purgeme")
 async def purgeme(delme):
     message = delme.text
     count = int(message[9:])
