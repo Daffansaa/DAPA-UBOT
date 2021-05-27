72e2566e047b1ff90c8b61321c2c0cf297b6df40
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-05-27 20:48:12 +0700

    huaaa

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -10,7 +10,7 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.cspam (.*)")
+@register(outgoing=True, pattern="^Cspam (.*)")
 async def tmeme(e):
     cspam = str(e.pattern_match.group(1))
     message = cspam.replace(" ", "")
@@ -23,7 +23,7 @@ async def tmeme(e):
             "TSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.wspam (.*)")
+@register(outgoing=True, pattern="^Wspam (.*)")
 async def tmeme(e):
     wspam = str(e.pattern_match.group(1))
     message = wspam.split()
@@ -36,7 +36,7 @@ async def tmeme(e):
             "WSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^spam (.*)")
+@register(outgoing=True, pattern="^Spam (.*)")
 async def spammer(e):
     counter = int(e.pattern_match.group(1).split(' ', 1)[0])
     spam_message = str(e.pattern_match.group(1).split(' ', 1)[1])
@@ -47,7 +47,7 @@ async def spammer(e):
                                     "Spam was executed successfully")
 
 
-@register(outgoing=True, pattern="^.picspam")
+@register(outgoing=True, pattern="^Picspam")
 async def tiny_pic_spam(e):
     message = e.text
     text = message.split()
@@ -62,7 +62,7 @@ async def tiny_pic_spam(e):
             "PicSpam was executed successfully")
 
 
-@register(outgoing=True, pattern="^delayspam (.*)")
+@register(outgoing=True, pattern="^Delayspam (.*)")
 async def spammer(e):
     spamDelay = float(e.pattern_match.group(1).split(' ', 2)[0])
     counter = int(e.pattern_match.group(1).split(' ', 2)[1])
@@ -79,15 +79,15 @@ async def spammer(e):
 
 CMD_HELP.update({
     "spam":
-    "`.cspam` <text>\
+    "`Cspam` <text>\
 \nUsage: Spam the text letter by letter.\
-\n\n`spam` <count> <text>\
+\n\n`Spam` <count> <text>\
 \nUsage: Floods text in the chat !!\
-\n\n`.wspam` <text>\
+\n\n`Wspam` <text>\
 \nUsage: Spam the text word by word.\
-\n\n`.picspam` <count> <link to image/gif>\
+\n\n`Picspam` <count> <link to image/gif>\
 \nUsage: As if text spam was not enough !!\
-\n\n`delayspam` <delay> <count> <text>\
-\nUsage: `.bigspam` but with custom delay.\
+\n\n`Delayspam` <delay> <count> <text>\
+\nUsage: `Bigspam` but with custom delay.\
 \n\n\nNOTE : Spam at your own risk !!"
 })
