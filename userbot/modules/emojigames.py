be9006382ea0d78e8514b7985cb8fe26494843c5
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-03 12:27:16 +0700

    biasalah

--- userbot/modules/emojigames.py
+++ userbot/modules/emojigames.py
@@ -40,7 +40,7 @@ async def _(event):
             pass
 
 
-@register(outgoing=True, pattern="^.bola(?: |$)(.*)")
+@register(outgoing=True, pattern="^.basket(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -56,8 +56,59 @@ async def _(event):
         except BaseException:
             pass
 
+
+@register(outgoing=True, pattern="^.bola(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('⚽'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('⚽'))
+        except BaseException:
+            pass
+
+
+@register(outgoing=True, pattern="^.gabruk(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('🎳'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('🎳'))
+        except BaseException:
+            pass
+
+
+@register(outgoing=True, pattern="^.hoki(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    await event.delete()
+    r = await event.reply(file=InputMediaDice('🎰'))
+    if input_str:
+        try:
+            required_number = int(input_str)
+            while not r.media.value == required_number:
+                await r.delete()
+                r = await event.reply(file=InputMediaDice('🎰'))
+        except BaseException:
+            pass
+
 CMD_HELP.update({
     "emojigames":
-    "`.dadu` 1-6 or `.lt`1-6 or `.bola`1-5\
-\nUsage: hahaha just a magic.\nWarning:`Don't use any other values or bot will crash`"
+    "`.dadu` 1-6 or `.lt`1-6 or `.bola`1-5 or `.basket`1-5 or `.gabruk`1-5 or `.hoki`1-5\
+\nUsage: Itu Hanya Game emoji yang bisa kalian mainkan beramai ramai`"
 })
