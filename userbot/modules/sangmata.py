4466c0667e60d1e52df78a3109a3bf69d30925b5
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-04 03:44:16 +0700

    ❌

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -16,14 +16,14 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Reply Ke Pesan Pengguna Yang Ingin Anda Scan.```")
+        await steal.edit("```Mohon Reply Ke Pesan Pengguna Dulu ngentot```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Reply Ke Pesan Pengguna Yang Ingin Di Scan.```")
+        await steal.edit("```Reply Ke Pesan Pengguna dulu ngentot```")
         return
     await steal.edit("LU SIAPA SI KENTOT,GUA KEPO, GA SENENG PC!!.")
     try:
@@ -47,7 +47,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Saya Tidak Menemukan Informasi Pergantian Nama Ini, Orang Ini Belum Pernah Mengganti Nama Sebelumnya```")
+                await steal.edit("```Masa Gua Gak Nemu Riwayat Namanya si ngentot, Wah si anjing belom pernah ganti nama kayak nya su!!```")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
