1533ebe01186512b09ad60ac5d452cccb4ed61bf
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-04 12:05:56 +0700

    mengtest

--- userbot/modules/ramgames.py
+++ userbot/modules/ramgames.py
@@ -40,6 +40,18 @@ async def _(event):
     await tap[0].click(event.chat_id)
     await event.delete()
 
+@register(outgoing=True, pattern=r"^\.mod(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@truthordares_bot"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr)
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
 CMD_HELP.update({
     "games": "\
 𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.xogame`\
