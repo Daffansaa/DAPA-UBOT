00b2eb3b42477960a50fa708db3c253d248efcf6
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-04 12:48:49 +0700

    hwah

--- userbot/modules/ramgames.py
+++ userbot/modules/ramgames.py
@@ -48,7 +48,7 @@ async def _(event):
     noob = "play"
     if event.reply_to_msg_id:
         await event.get_reply_message()
-    tap = await bot.inline_query(botusername, modr)
+    tap = await bot.inline_query(botusername, noob)
     await tap[0].click(event.chat_id)
     await event.delete()
 
