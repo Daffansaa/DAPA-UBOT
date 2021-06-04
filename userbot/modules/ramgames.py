af4c11bf49b53347f03b97b2750eae2a159c58bd
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-04 12:40:35 +0700

    Update ramgames.py

--- userbot/modules/ramgames.py
+++ userbot/modules/ramgames.py
@@ -44,8 +44,8 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    modr = event.pattern_match.group(1)
     botusername = "@gamee"
+    noob = "play"
     if event.reply_to_msg_id:
         await event.get_reply_message()
     tap = await bot.inline_query(botusername, modr)
