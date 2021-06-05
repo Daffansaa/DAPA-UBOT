a69dd310661f72914ffcfcf51cfece67f61156d7
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-05 09:58:23 +0700

    oooasuuu

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -386,7 +386,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} 🌟".format("🌟", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} 💫".format("💫", x), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
@@ -482,7 +482,7 @@ with bot:
                 current_page_number = int(
                     event.data_match.group(1).decode("UTF-8"))
                 buttons = paginate_help(
-                    current_page_number + 1, dugmeler, "helpmel")
+                    current_page_number + 1, dugmeler, "helpme")
                 # https://t.me/TelethonChat/115200
                 await event.edit(buttons=buttons)
             else:
