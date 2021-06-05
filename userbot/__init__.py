d117f57d2dc1a35bc4a103ffdf7423d0310b80f7
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-05 12:04:22 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -382,7 +382,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 
 def paginate_help(page_number, loaded_modules, prefix):
     number_of_rows = 5
-    number_of_cols = 1
+    number_of_cols = 4
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
@@ -390,7 +390,7 @@ def paginate_help(page_number, loaded_modules, prefix):
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
-                     modules[1::number_of_cols],
+                     modules[2::number_of_cols],
                      modules[2:number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
