c81d6780919fc59eb3d9594484321fdd21a55ef6
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-11 23:04:34 +0700

    fixing carbon

--- userbot/modules/ramcarbon.py
+++ userbot/modules/ramcarbon.py
@@ -75,8 +75,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by : [ラマダニ。](https://t.me/maafgausahsokap/),\
+        \na Group support : [Geez Support](https://t.me/geezsupportgroup/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -141,11 +141,10 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by : [ラマダニ。](https://t.me/maafgausahsokap/),\
+        \na Group support : [Geez Support](https://t.me/geezsupportgroup/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
-    )
 
     os.remove('/root/userbot/.bin/carbon.png')
     driver.quit()
@@ -207,8 +206,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by : [ラマダニ。](https://t.me/maafgausahsokap/),\
+        \na Group support : [Geez Support](https://t.me/geezsupportgroup/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -273,8 +272,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by : [ラマダニ。](https://t.me/maafgausahsokap/),\
+        \na Group support : [Geez Support](https://t.me/geezsupportgroup/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -339,8 +338,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by : [ラマダニ。](https://t.me/maafgausahsokap/),\
+        \na Group support : [Geez Support](https://t.me/geezsupportgroup/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
