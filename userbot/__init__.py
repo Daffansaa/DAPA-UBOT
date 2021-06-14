d25f2ce97a7eb3eec1539d14636a7532ceb54e50
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-15 04:49:45 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -440,7 +440,7 @@ with bot:
             try:
                 tgbotusername = BOT_USERNAME
                 if tgbotusername is not None:
-                    results = await event.client.inline_query(tgbotusername, "@UserButt")
+                    results = await event.client.inline_query(tgbotusername, "@Ram_ubot")
                     await results[0].click(
                         event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                     )
