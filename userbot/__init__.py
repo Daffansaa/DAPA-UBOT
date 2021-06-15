ce8659b105911788a07374c8db9a7d979de8bb72
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-15 07:12:27 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -396,10 +396,10 @@ def paginate_help(page_number, loaded_modules, prefix):
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols],
-                     modules[2::number_of_cols],
-                     modules[1::number_of_cols]))
-    if len(modules) % number_of_cols == 0:
-        pairs.append((modules[-1],))
+                     modules[1::number_of_cols],
+                     modules[2::number_of_cols]))
+    if len(modules) % number_of_cols == 2:
+        pairs.append((modules[-2],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
