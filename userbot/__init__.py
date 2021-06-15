8d7e86866128551eb00984cf42e96c5fdfcbac8e
Ram <80225366+ramadhani892@users.noreply.github.com>
2021-06-15 17:05:52 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -560,7 +560,6 @@ with bot:
                     buttons=[
                           Button.url("⭐𝗥𝗔𝗠-𝗨𝗕𝗢𝗧⭐", "t.me/ootspambot"),
                           Button.url("•SUPPORT• ", "t.me/GeezSupportGroup")
-                          Button.inline("OPEN BACK", data="{}_open({})".format(prefix, modulo_page)
                     ]
                 )
 
