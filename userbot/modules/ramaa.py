a350f6f12df4790ed0058abd87cb69351c59aae8
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-06-17 23:28:54 +0700

    wait

--- userbot/modules/ramaa.py
+++ userbot/modules/ramaa.py
@@ -55,6 +55,11 @@ async def typewriter(typew):
     await typew.edit("**Mas Ade,Bandar Bokep☑️**")
     await typew.edit("**Mas Ade,Bandar Bokep✅**")
     sleep(3)
+    await typew.edit("**Mas Ade ,Bandar Bokep☑️**")
+    await typew.edit("**Mas Ade ,Bandar Bokep**")
+    sleep(3)
+
+
     await typew.edit("**CUMA DAPA DOANG YANG BENER!**")
 
 # Create by myself @localheart
