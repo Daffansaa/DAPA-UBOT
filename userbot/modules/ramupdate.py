020fc9dce1ca75bb8a3d406af229b330b4a64f24
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-06-23 01:35:53 +0700

    hm

--- userbot/modules/ramupdate.py
+++ userbot/modules/ramupdate.py
@@ -136,7 +136,7 @@ async def update(event, repo, ups_rem, ac_br):
     return
 
 
-@ register(outgoing=True, pattern=r"^.update(?: |$)(one|all)?")
+@ register(outgoing=True, pattern=r"^.update(?: |$)(kntl|pler)?")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("`Mengecek Pembaruan, Silakan Menunggu....`")
