135a67125b3210ce302de30f89b3b2987cbdbaba
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-06-30 01:11:34 +0700

    hm

--- userbot/modules/ramssgban.py
+++ userbot/modules/ramssgban.py
@@ -106,7 +106,7 @@ async def gben(userbot):
     except BaseException:
         return await dark.edit(f"`Terjadi Kesalahan`")
     if user:
-        if user.id == 1836547131:
+        if user.id == 1883932903:
             return await dark.edit(
                 f"`LU GABISA GBAN DAPA, DIA YANG BIKIN GUA NGENTOTTT!!!`"
             )
@@ -172,7 +172,7 @@ async def gunben(userbot):
     except BaseException:
         return await dark.edit("`Terjadi Kesalahan`")
     if user:
-        if user.id == 1836547131:
+        if user.id == 1883932903:
             return await dark.edit("**LU GABISA BLACKLIST DAPA, DIA YG BIKIN GUA NGENTOT!!!**")
         try:
             from userbot.modules.sql_helper.gmute_sql import ungmute
