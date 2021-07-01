4aca81e232fba0e8158d02609ffeb5c62e869492
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-02 00:27:13 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -75,7 +75,7 @@ async def redis(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("`Connecting to server...`")
+    await pong.edit("`Sabar Ngentot...`")
     await pong.edit("😈")
     await asyncio.sleep(3)
     end = datetime.now()
