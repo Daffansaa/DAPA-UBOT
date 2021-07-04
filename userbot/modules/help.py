791170f0877e58917ab58ec37dd73c659626fcb0
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-05 00:31:11 +0700

    hm

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,11 +32,11 @@ async def help(event):
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t 💫  "
-        await event.edit("**⭐𝐃𝐀𝐏𝐀-𝐔𝐁𝐎𝐓⭐**\n\n"
-                         f"**💫 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**💫  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
-                         "**💫 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\n"
-                         f"💫 {string}\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+            string += "`\t 🐯  "
+        await event.edit("**🐯𝐃𝐀𝐏𝐀-𝐔𝐁𝐎𝐓🐯**\n\n"
+                         f"🐯 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**🐯  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
+                         "**🐯 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\n"
+                         f"🐯 {string}\n\n ═════════════╣❃ ♕ ❃╠═════════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
         await event.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
         await asyncio.sleep(50)
         await event.delete()
