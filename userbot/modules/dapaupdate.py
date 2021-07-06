d119f92635d15961638b3c3f275fdcd8a7df83b7
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-07 01:48:41 +0700

    Update dapaupdate.py

--- userbot/modules/dapaupdate.py
+++ userbot/modules/dapaupdate.py
@@ -194,7 +194,7 @@ async def upstream(event):
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk 🐯DAPA-UBOT🐯 [{ac_br}]:\n\n💻Update:**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk 🐯DAPA-UBOT🐯 [{ac_br}]:\n\n💻 Updater:**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
