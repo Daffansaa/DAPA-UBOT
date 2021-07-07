0e207bdd9c7a987d8133da71948de76879721e69
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-08 03:06:59 +0700

    ALHAMDULILLAH

--- userbot/modules/dapacarbon.py
+++ userbot/modules/dapacarbon.py
@@ -343,8 +343,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        caption="Created by [DAPA](https://t.me/MadBoyys/),\
+        \nGroup Support [Group Spam](https://t.me/ootspambot)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
