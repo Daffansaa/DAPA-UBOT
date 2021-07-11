fdf1080468a4d7f569076ff94d00053bc04b80ef
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-12 01:26:21 +0700

    .

--- userbot/modules/dapaaa.py
+++ userbot/modules/dapaaa.py
@@ -112,6 +112,6 @@ CMD_HELP.update({
     \nUsage: menampilkan alive bot.\
     \n\n`.sadboy`\
     \nUsage: hiks\
-    \n\n`.punten` ; `.geez`\
+    \n\n`.punten` ; `.404`\
     \nUsage: misi."
 })
