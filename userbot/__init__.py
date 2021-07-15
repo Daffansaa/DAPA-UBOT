e843a531d05088957906c85224e80d69452964a5
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-15 11:36:11 +0700

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -520,7 +520,7 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@Ram_ubot"):
+            if event.query.user_id == uid and query.startswith("@Dapuserbot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=daplogo,
