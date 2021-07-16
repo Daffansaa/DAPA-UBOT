92de958ec620ad72c2d895e2a0d72ac7c6435786
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-16 20:42:14 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -175,7 +175,7 @@ async def kang(args):
                         return await args.edit(
                             "`Sticker telah dibuat ke pack baru !"
                             "\nIni Pack Yang Baru Saja Anda Buat !"
-                            f"Berhasil Mencuri!\n\n╭✠━━━━❖━━━━✠╮\n            [TEKAN](t.me/addstickers/{packname})\n╰✠━━━━❖━━━━✠╯\n\nUntuk Melihat Sticker Anda.",
+                            f"Berhasil Mencuri!\n\n╭✠━━━━❖━━━━✠╮\n          [TEKAN](t.me/addstickers/{packname})\n╰✠━━━━❖━━━━✠╯\n\nUntuk Melihat Sticker Anda.",
                             parse_mode="md",
                         )
                 if is_anim:
