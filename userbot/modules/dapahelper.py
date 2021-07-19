8e3744abb0da72c8ca300515072a9c052e0d9b4c
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-20 01:26:43 +0700

    .

--- userbot/modules/dapahelper.py
+++ userbot/modules/dapahelper.py
@@ -21,7 +21,7 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/ramadhani892/RAM-UBOT/RAM-UBOT/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Daffansaa/DAPA-UBOT/DAPA-UBOT/varshelper.txt)")
 
 
 CMD_HELP.update({
