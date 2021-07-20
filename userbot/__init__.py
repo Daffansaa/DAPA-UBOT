47978db27dfdae06afac1d8f392eafbf413ff41c
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-21 01:57:01 +0700

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -523,23 +523,23 @@ with bot:
             result = None
             query = event.text
             if event.query.user_id == uid and query.startswith("@Dapuserbot"):
-                buttons = [
-                    (Button.inline("Open Main Menu", data="mainmenu"),),
-                ]
-                photo_bytesio = daplogo
-                result = builder.photo(photo_bytesio,
+                buttons = paginate_help(0, dugmeler, "helpme")
+                result = builder.photo(
+                    file=daplogo,
                     link_preview=False,
-                    text=f"\n**Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n\n**NGENTOT**",
+                    text=f"{REPO_NAME}\n\n𝗣𝗘𝗠𝗜𝗟𝗜𝗞 𝗕𝗢𝗧 : {DEFAULTUSER}\n\n🐯 𝗩𝗘𝗥𝗦𝗜 𝗕𝗢𝗧 : `7.0`\n🐯 𝗠𝗢𝗗𝗨𝗟𝗘𝗦 : `{len(plugins)}`\n\n🔥 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 : [{DEFAULTUSER}]({OWNER_BOT}) ".format(
+                        len(dugmeler),
+                    ),
                     buttons=buttons,
                 )
             elif query.startswith("tb_btn"):
                 result = builder.article(
-                    "Bantuan Dari 🐯𝗗𝗔𝗣𝗔-𝗨𝗕𝗢𝗧🐯 ",
+                    f"Bantuan Dari {REPO_NAME} ",
                     text="Daftar Plugins",
                     buttons=[],
-                    link_preview=False)
+                    link_preview=True)
             else:
-                 result = builder.article(
+                result = builder.article(
                     f" ━━━━━━━❖━━━━━━━━\n       {REPO_NAME}\n━━━━━━━❖━━━━━━━━ ",
                     text=f"""**━━━━━━━❖━━━━━━━━\nLU BIKIN \n{REPO_NAME}\nSENDIRI DONG NGENTOT!!\n━━━━━━━❖━━━━━━━━\nNIH CARANYA:**__TEKEN DIBAWAH INI NGENTOT!__ 👇""",
                     buttons=[
@@ -550,31 +550,11 @@ with bot:
                             custom.Button.url(
                                 "Daffansaa",
                                 f"{OWNER_BOT}")] 
-                    link_preview=True,
+                    ],
+                    link_preview=False,
                 )
             await event.answer([result] if result else None)
 
-
-                                       
-        @tgbot.on(
-            events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-                data=re.compile(rb"mainmenu")
-            )
-        )
-        async def on_plug_in_callback_query_handler(event):
-            if event.query.user_id == uid:
-                buttons = paginate_help(0, dugmeler, "helpme")
-                text=f"{REPO_NAME}\n\n𝗣𝗘𝗠𝗜𝗟𝗜𝗞 𝗕𝗢𝗧 : {DEFAULTUSER}\n\n🐯 𝗩𝗘𝗥𝗦𝗜 𝗕𝗢𝗧 : `7.0`\n🐯 𝗠𝗢𝗗𝗨𝗟𝗘𝗦 : `{len(plugins)}`\n\n🔥 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 : [{DEFAULTUSER}]({OWNER_BOT}) ".format(
-                await event.edit(text,
-                    file=daplogo,
-                    buttons=buttons,
-                    link_preview=False,
-                )
-            else:
-                reply_pop_up_alert = f"WOI NGENTOT!! JANGAN PAKE PUNYA {DEFAULTUSER} DONG BABI."
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-                                       
-                                       
         @tgbot.on(
              events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                  data=re.compile(rb"nepo")
