0d2f235c02011e16db72526811d13441b8012063
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-07-25 02:48:59 +0700

    .

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -605,7 +605,7 @@ with bot:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
-       @tgbot.on(
+        @tgbot.on(
             events.callbackquery.CallbackQuery(  # pylint:disable=E0602
                 data=re.compile(rb"helpme_close\((.+?)\)")
             )
