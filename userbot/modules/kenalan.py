00daded713b52527fe1d6db0dcaa8d2a832e9090
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-11-18 01:29:10 +0700

    Update kenalan.py

--- userbot/modules/kenalan.py
+++ userbot/modules/kenalan.py
@@ -8,7 +8,7 @@ async def typewriter(typew):
     sleep(1)
     await typew.edit("`Hai Perkenalkan Namaku Daffansaa`")
     sleep(3)
-    await typew.edit("`16 Tahun`")
+    await typew.edit("`17 Tahun`")
     sleep(1)
     await typew.edit("`Tinggal Di Bandung, Salam Kenal Ya Ngentot...`")
 # Create by myself @localheart
