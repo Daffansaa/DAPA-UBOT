abf9f4c16d705e3de1337daec49c36670051cee9
Daffansaa <85962930+Daffansaa@users.noreply.github.com>
2021-11-21 01:00:18 +0700

    UPDATE GAIS

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -2,7 +2,7 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 # Credits @keselekpermen69 / @Ultroid / @DAPA-UBOT
-# Ported @MaafGausahSokap / JANGAN DI APUS BABI
+# Ported @Daffansaa / JANGAN DI APUS BABI
 """Userbot initialization."""
 
 
